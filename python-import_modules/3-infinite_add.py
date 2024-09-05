3f2adf953cfdcf638538b1d06a181ae46aa39041
SadScourge <darklzar@gmail.com>
2024-09-05 17:13:13 +0200

    added every project 4 exercises

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+index = 1
+result = 0
+
+if len(sys.argv) == 1:
+	pass
+else:
+	while index < len(sys.argv):
+		result += int(sys.argv[index])
+		index += 1
+	print (f"{result}")
+	
