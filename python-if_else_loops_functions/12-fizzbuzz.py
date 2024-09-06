77ffa45e318d332f563cc36726d1b59687c964a0
SadScourge <darklzar@gmail.com>
2024-09-06 23:09:54 +0200

    corrected output

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,10 @@
 def fizzbuzz():
     for count in range(1, 101):
         if ((count % 3 == 0) and (count % 5 == 0)):
-            print("FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif (count % 3 == 0):
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         elif (count % 5 == 0):
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         else:
-            print(f"{count}", end="")
-        if count != 100:
-            print(" ", end="")
+            print(f"{count}", end=" ")
