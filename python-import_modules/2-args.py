64ebfdf2d350e2a8aa340bb948fee57f74ba2813
SadScourge <darklzar@gmail.com>
2024-09-06 23:34:48 +0200

    added guard to files 2 & 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-
-index = 1
-
-if len(sys.argv) == 1:
-    print("0 argument.")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    while index < len(sys.argv):
-        print("{}: {}".format(index, sys.argv[index]))
-        index += 1
+if __name__ == "__main__":
+    import sys
+    
+    index = 1
+    
+    if len(sys.argv) == 1:
+        print("0 argument.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        while index < len(sys.argv):
+            print("{}: {}".format(index, sys.argv[index]))
+            index += 1
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-index = 1
-result = 0
+    index = 1
+    result = 0
 
-if len(sys.argv) == 1:
-    pass
-else:
-    while index < len(sys.argv):
-        result += int(sys.argv[index])
-        index += 1
-    print("{}".format(result))
+    if len(sys.argv) == 1:
+        pass
+    else:
+        while index < len(sys.argv):
+            result += int(sys.argv[index])
+            index += 1
+        print("{}".format(result))
