532afc255f6d186002bbbfae80f2f2da50fd1e68
SadScourge <darklzar@gmail.com>
2024-09-06 22:41:06 +0200

    corrected output (excess newline)

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 result = ""
 for index in range(0, 99):
-    result += f"{index} = {hex(index)}\n"
+    result += f"{index} = {hex(index)}"
+    if index != 98:
+        result += "\n"
 print("{}".format(result))
