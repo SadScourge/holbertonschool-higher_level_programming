846b549e7dd2d6a316418efdbddbc3663323e4b2
SadScourge <darklzar@gmail.com>
2024-09-13 21:35:53 +0200

    corrected each file for the checker

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    for index1 in range(len(matrix)):
-        for index2 in range(len(matrix[0])):
-            print("{}".format(matrix[index1][index2]), end="")
-            if index2 != len(matrix[0]):
-                print(" ", end="")
-        if index1 != len(matrix):
-            print("\n", end="")
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
