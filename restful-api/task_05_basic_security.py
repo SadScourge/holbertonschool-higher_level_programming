7bbf23106e93153213846cefcfe1fb1fcaaf2234
SadScourge <darklzar@gmail.com>
2024-10-18 23:05:41 +0200

    changed the password for both of my users

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -18,11 +18,11 @@ jwt = JWTManager(app)
 users = {
             "user1": {
                 "username": "user1",
-                "password": generate_password_hash("azerty"),
+                "password": generate_password_hash("password"),
                 "role": "user"},
             "admin1": {
                 "username": "admin1",
-                "password": generate_password_hash("admin"),
+                "password": generate_password_hash("password"),
                 "role": "admin"}
         }
 
