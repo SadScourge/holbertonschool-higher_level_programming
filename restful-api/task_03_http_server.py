7a3455b4b73e1172e2b3d1fb206071610c8441a9
SadScourge <darklzar@gmail.com>
2024-10-18 20:25:35 +0200

    added content type to some pages and changed encode for json to 'utf-8'

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -11,13 +11,14 @@ class Holberton_API(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
+            self.send_header("Content-type", "application/json")
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps({"name": "John", "age": 30,
-                                         "city": "New York"}).encode())
+                                         "city": "New York"}).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
             self.wfile.write(b"OK")
@@ -28,9 +29,11 @@ class Holberton_API(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps({
                 "version": "1.0",
                 "description": "A simple API built with http.server"
-                }).encode())
+                }).encode('utf-8'))
         else:
             self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
