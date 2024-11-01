f9cdd5f61b64b3eaf05633e7e6f18b3020474e46
SadScourge <darklzar@gmail.com>
2024-11-01 20:31:11 +0100

    Corrected somes typo and bad logic

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states
-ON cities.state_id
+LEFT JOIN states
+ON states.id = cities.state_id
 ORDER BY cities.id ASC;
